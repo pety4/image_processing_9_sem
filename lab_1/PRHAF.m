b557d2211dcb4588e3d3cb5840ee2d664882f49e
Peter <peter_orl@mail.ru>
2024-09-22 14:07:33 +0300

    лабораторная работа №1

--- lab_1/PRHAF.m
+++ lab_1/PRHAF.m
@@ -3,21 +3,19 @@ function [H, theta, rho] = PRHAF(I, dtheta, drho)
     %I,H - входное и выходное изображения
     %dtheta, drho - шаги дискретизации плоскости Хафа
     %theta, rho - вектор значений параметров плоскости Хафа
-    theta = -90:dtheta:90;
     [M, N] = size(I);
     D = sqrt((M-1).^2 + (N-1).^2);
-    rho = -D:drho:D;
-    n_theta = length(theta);
-    n_rho = length(rho);
+    n_theta = real_n_count(-90, 90, dtheta);
+    n_rho = real_n_count(-D, D, drho);
+    theta = linspace(-90, 90, n_theta);
+    rho = linspace(-D, D, n_rho);
+
     H = zeros(n_rho, n_theta);
     [x,y,q] = null_pixel_count(I);
     for k=1:q
         for j=1:n_theta
             rho_count = x(k)*cos(theta(j)*pi/180) + y(k)*sin(theta(j)*pi/180);
             i = floor(((rho_count-rho(1))/drho)+1);
-%             if i > 283
-%                 i=283;
-%             end
             H(i, j) = H(i,j) + 1;
         end
     end
--- lab_1/main.m
+++ lab_1/main.m
@@ -1,53 +1,38 @@
 clear all;
 close all;
-image = image_create_var_7();
-%figure();
-%imshow(image);
+%% Инициализация начальных переменных
+global M  N x_1 y_1 x_2 y_2 x_min x_max;
+init_var_7();
 dtheta = 0.5;
-drho = 1;%TODO - добавить определение шага из методички стр 75
-
+drho = 1;
+%% Построение исходного изображения
+image = image_with_line_based_on_two_points_create(M, N, x_1, x_2, y_1, y_2, x_min, x_max);
+figure();
+imshow(image);
+xlabel('y'), ylabel('x'); 
+axis on;
+title('Исходное изображение');
+%% Построение проекции исходного изображения на плоскость Хафа
 [image_haf, theta, rho] = PRHAF(image, dtheta, drho);
 figure();
 imshow(image_haf,[],'XData',theta,'YData',rho,'InitialMagnification','fit');
 xlabel('\theta'), ylabel('\rho'); 
 axis on, axis normal;
-
+title('Проекция исходного изображения на плоскость Хафа')
+%% Определение коэффициентов уравнения прямой
 [rho_max_index, theta_max_index] = find(image_haf == max(image_haf,[],"all"));
 rho_max = rho(rho_max_index(1));
 theta_max = theta(theta_max_index(1));
 a_max = -cos(theta_max*pi/180)/sin(theta_max*pi/180);
 b_max = rho_max/sin(theta_max*pi/180);
-x_min = 60;
-x_max = 100;
-
-y_min =  a_max*x_min+b_max;
-y_max = a_max*x_max+b_max;
-
-alpha=-90;
-x_min_new=x_min*cosd(alpha)-y_min*sind(alpha);
-y_min_new=x_min*sind(alpha)-y_min*cosd(alpha);
-
-x_max_new=x_max*cosd(alpha)-y_max*sind(alpha);
-y_max_new=x_max*sind(alpha)-y_max*cosd(alpha);
-
-a_new = (y_max_new - y_min_new)/(x_max_new - x_min_new);
-b_new = y_min_new - a_new*x_min_new;
-
-x_new = 0:101;
-y_new = a_new*x_new+b_new;
-
-% for x = x_min:1:x_max
-%     y = a_max*x+b_max;
-%     image(x +1, floor(y+1)) = 0;
-% end
-
-new_image = image_create_a_b(101,101,a_max,b_max,x_min,x_max);
+%% Построение прямой на исходном изображении
+for x = x_min:1:x_max
+     y = a_max*x+b_max;
+     image(x +1, floor(y+1)) = 0;
+end
 figure();
 imshow(image);
-hold on;
-plot(x_new, y_new+100);
-%plot(y,x);
-%figure();
-%imshow(new_image);
-
+xlabel('y'), ylabel('x'); 
+axis on;
+title('Полученное изображение');
 
